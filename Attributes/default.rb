3908034ffe3bec5d97f844c42d13368c01a71d76
maurosaporita <mauro.saporita@eaglegenomics.com>
2018-10-03 10:56:53 +0100

    first working version

--- Attributes/default.rb
+++ Attributes/default.rb
@@ -4,8 +4,8 @@
 #
 # Copyright:: 2018, Eagle Genomics Ltd, All Rights Reserved.
 
-default['mapseq']['version'] = '1.1.11'
-default['mapseq']['base_name'] = 'mapseq-' + node['mapseq']['version']
+default['mapseq']['version'] = '1.2.2'
+default['mapseq']['base_name'] = 'mapseq-' + node['mapseq']['version'] + '-linux'
 default['mapseq']['archive'] = node['mapseq']['base_name'] + '.tar.gz'
 default['mapseq']['install_dir'] = '/usr/local'
 default['mapseq']['dir'] = node['mapseq']['install_dir'] + '/' + node['mapseq']['base_name']
--- recipes/default.rb
+++ recipes/default.rb
@@ -8,8 +8,16 @@ include_recipe 'apt'
 include_recipe 'build-essential'
 include_recipe 'tar'
 
-tar_extract "foo" do
-  source node['mapseq']['download_url']
+tar_extract "#{node['mapseq']['download_url']}" do
   target_dir node['mapseq']['install_dir']
   creates node['mapseq']['dir']
+end
+
+magic_shell_environment 'PATH' do
+  filename 'mapseq'
+  value "$PATH:#{node['mapseq']['install_dir']}"
+end
+
+magic_shell_environment 'MAPSEQ_VERSION' do
+  value node['mapseq']['version']
 end
